7b8adad8dca72adb747e869648e20a015d91aed8
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2020-07-25 11:44:42 +0200

    Multiselectable list boxes.

--- modules/lists.js
+++ modules/lists.js
@@ -2,7 +2,8 @@ class List {
   constructor(root) {
     this.root = root;
 
-    this.isIconGrid = this.root.children.length > 0 && this.root.children[0].classList.contains('icon-grid');
+    this.multiselect = this.root.getAttribute('aria-multiselectable') === 'true';
+    this.iconGrid = this.root.children.length > 0 && this.root.children[0].classList.contains('icon-grid');
     this.options = this.root.querySelectorAll('[role="option"]');
     this.selectedOption = -1;
     this.prefix = '';
@@ -12,22 +13,22 @@ class List {
       const option = this.options[i];
       option.addEventListener('click', e => {
         e.stopPropagation();
-        this.select(i);
+        e.preventDefault();
+        this.select(i, e.shiftKey, e.ctrlKey, true);
       });
       option.addEventListener('keydown', e => {
         if (e.key === ' ' && !this.prefix) {
           e.preventDefault();
-          this.select(i);
-        } else if (e.key.length === 1) {
+          this.select(i, e.shiftKey, true, true);
+        } else if (e.key.length === 1 && !e.ctrlKey && !e.metaKey && !e.altKey) {
           e.preventDefault();
           if (this.prefixTimeout) {
             clearTimeout(this.prefixTimeout);
-            this.prefixTimeout = null;
           }
           this.prefix += e.key.toLowerCase();
           for (let j = 0; j < this.options.length; j++) {
             if (this.options[j].textContent.trimLeft().toLowerCase().startsWith(this.prefix)) {
-              this.select(j);
+              this.select(j, false, false);
               break;
             }
           }
@@ -37,11 +38,11 @@ class List {
           }, 700);
         } else if (e.key === 'Home') {
           e.preventDefault();
-          this.select(0);
+          this.select(0, e.shiftKey, e.ctrlKey);
         } else if (e.key === 'End') {
           e.preventDefault();
-          this.select(this.options.length - 1);
-        } else if (this.isIconGrid) {
+          this.select(this.options.length - 1, e.shiftKey, e.ctrlKey);
+        } else if (this.iconGrid) {
           if (this.options.length > 0) {
             let columns;
             const y0 = this.options[0].getBoundingClientRect().top;
@@ -55,31 +56,31 @@ class List {
             const y = (i / columns) | 0;
             if (e.key === 'ArrowDown') {
               e.preventDefault();
-              this.select(x + (y + 1) * columns);
+              this.select(x + (y + 1) * columns, e.shiftKey, e.ctrlKey);
             } else if (e.key === 'ArrowUp') {
               e.preventDefault();
-              this.select(x + (y - 1) * columns);
+              this.select(x + (y - 1) * columns, e.shiftKey, e.ctrlKey);
             } else if (e.key === 'ArrowLeft') {
               e.preventDefault();
               if (x > 0) {
-                this.select(i - 1);
+                this.select(i - 1, e.shiftKey, e.ctrlKey);
               }
             } else if (e.key === 'ArrowRight') {
               e.preventDefault();
               if (x < columns - 1) {
-                this.select(i + 1);
+                this.select(i + 1, e.shiftKey, e.ctrlKey);
               }
             }
           }
         } else if (e.key === 'ArrowDown') {
           e.preventDefault();
-          this.select(i + 1);
+          this.select(i + 1, e.shiftKey, e.ctrlKey);
         } else if (e.key === 'ArrowUp') {
           e.preventDefault();
-          this.select(i - 1);
+          this.select(i - 1, e.shiftKey, e.ctrlKey);
         }
       });
-      if (!option.getAttribute('aria-selected')) {
+      if (option.getAttribute('aria-selected') !== 'true') {
         option.setAttribute('tabindex', -1);
       } else {
         this.selectedOption = i;
@@ -90,6 +91,11 @@ class List {
       this.options[0].setAttribute('tabindex', 0);
     }
     this.root.addEventListener('click', () => {
+      if (this.multiselect) {
+        for (let option of this.options) {
+          option.setAttribute('aria-selected', false);
+        }
+      }
       if (this.selectedOption >= 0) {
         this.options[this.selectedOption].focus();
       } else if (this.options.length) {
@@ -98,19 +104,44 @@ class List {
     });
   }
 
-  select(index) {
+  select(index, shift, ctrl, click) {
     if (index < 0 || index >= this.options.length) {
       return;
     }
     const option = this.options[index];
     for (let other of this.options) {
-      other.setAttribute('aria-selected', false);
+      if (!this.multiselect || !ctrl) {
+        other.setAttribute('aria-selected', false);
+      }
       other.setAttribute('tabindex', -1);
     }
-    option.setAttribute('aria-selected', true);
+    if (this.multiselect && (ctrl || shift)) {
+      if (this.selectedOption < 0) {
+        this.selectedOption = index;
+      }
+      if (shift) {
+        if (this.selectedOption < index) {
+          for (let i = this.selectedOption; i <= index; i++) {
+            this.options[i].setAttribute('aria-selected', true);
+          }
+        } else {
+          for (let i = this.selectedOption; i >= index; i--) {
+            this.options[i].setAttribute('aria-selected', true);
+          }
+        }
+      } else if (ctrl && click) {
+        if (option.getAttribute('aria-selected') === 'true') {
+          option.setAttribute('aria-selected', false);
+        } else {
+          option.setAttribute('aria-selected', true);
+        }
+      }
+    } else {
+      option.setAttribute('aria-selected', true);
+      this.selectedOption = index;
+    }
     option.setAttribute('tabindex', 0);
     option.focus();
-    this.selectedOption = index;
   }
 }
 
